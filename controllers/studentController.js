f90994212b6902d3277d187bdefd4479265d88db
shalini <s.rathore43@gmail.com>
2025-06-06 10:42:38 +0530

    asdasda

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,14 +1,37 @@
 const studentModel = require("../models/studentModel")
 
-exports.insertStudent = (s)=>
+exports.insertStudent =async (s)=>
 {
+    var msg = "blank";
     let studnet = new studentModel({
         name:s.name,
         age:s.age,
         city:s.city
     })
-    studnet.save()
-    //   name:   String,
-    //     age:Number,
-    //     city:String    
+   await studnet.save()
+    .then(()=>{msg = "Record Inserted"})     
+    .catch((err)=>msg = "err occured")
+    let data = [];
+    if(msg=="Record Inserted")
+    {
+        await studentModel.find()
+        .then((d)=>data=d)
+    }
+    return {msg:msg,data:data }
+}
+exports.getStudents = async()=>
+{
+    let students = [];
+    await studentModel.find()
+    .then((d)=>students=d)
+    .catch((e)=>students = e)
+    return students
+}
+exports.getStudentById =async (id)=>
+{
+    let students = [];
+    await studentModel.findById(id)
+    .then((d)=>students=d)
+    .catch((e)=>students = e)
+    return students
 }
--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,13 +1,23 @@
 const express = require("express")
 const studentController = require("../controllers/studentController")
 const router = express.Router()
-router.post("/",(req,res)=>
+router.post("/",async(req,res)=>
 {
     let s = {name:req.body.name,
         age:parseInt(req.body.age)
         ,city:req.body.city}
-        studentController.insertStudent(s)
-        res.send("record added")
+        let m = await studentController.insertStudent(s)
+        res.send(m)
     })
-
+router.get("/",async(req,res)=>
+{
+    let students = await studentController.getStudents()
+    res.send({data:students})
+})
+router.get("/:id",async(req,res)=>
+{
+    let id = req.params.id
+    let students = await studentController.getStudentById(id)
+    res.send({data:students})
+})
 module.exports = router
