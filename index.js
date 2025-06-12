5f91c6556eccbcf8f5a135f5fb3473dd027ec7d9
shalini <s.rathore43@gmail.com>
2025-06-12 09:53:53 +0530

    fasdsa

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const dbconfig = require("./dbconfig")
 const studentRouter = require("./routers/studentRouter")
 const app = express()
+const cors = require("cors")
 // admin , user , category,produts
+
 const userRouter = require("./routers/userRouter")
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/user",userRouter)
